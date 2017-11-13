6b60e7e6e960925afba0372a8baedfeb9e14645d
ravi03k <ravi03k@gmail.com>
2017-11-13 09:49:38 +0530

    Initial commit

---
+++ Car-Pooling-master/ChangedFiles/MainApplication.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2016 Pooja, SriHarsha
+ * This code is available under the "MIT License".
+ * Please see the file LICENSE in this distribution
+ * for license terms.
+ */
+
+package com.example.sriharsha.carpool;
+
+import android.app.Application;
+
+import com.kinvey.android.Client;
+
+/**
+ * Created by Ravi on 7/30/2017.
+ */
+
+/**
+ * This class establishes the conneciton with the Kinvey mBass
+ */
+public class MainApplication extends Application {
+    private Client client;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        defineClient();
+    }
+
+    private void defineClient() {
+        client = new Client.Builder("kid_Hk-ehpryf",//APP_ID
+                "0f9d06b202f0445288696b956b243d7a",//APP_SECRET
+                getApplicationContext()).build();
+    }
+
+    public Client getClient() {
+        return client;
+    }
+}
